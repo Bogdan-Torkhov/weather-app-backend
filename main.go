08eeeeca84b561d51f61b589c2fc1145e5eb1162
noarainstorm <100447165+noarainstorm@users.noreply.github.com>
2022-06-09 11:23:36 +0300

    Add proxy

--- main.go
+++ main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"log"
 	"fmt"
-	"os"
+	// "os"
     owm "github.com/briandowns/openweathermap"
+    "net/http"
+    "net/url"
 )
 
-var apiKey = os.Getenv("OWM_API_KEY")
+// var apiKey = os.Getenv("OWM_API_KEY")
 
+const proxy = "http://38.55.182.255:80"
+
+const apiKey = "6ab7fc8050c5668c33576f3a93d1deea"
 
 func main() {
+<<<<<<< HEAD
+	i, err := url.Parse(proxy)
+	if err != nil { panic(err) }
+	cli := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(i)}}
+	w, err := owm.NewCurrent("C", "ru", apiKey, owm.WithHttpClient(cli)) // celseus (imperial) with Russian output
+=======
 	w, err := owm.NewCurrent("C", "RU", apiKey) // fahrenheit (imperial) with Russian output
+>>>>>>> refs/remotes/origin/main
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	w.CurrentByName("Murmansk")
-	fmt.Println(w)
+	log.Println(apiKey)
+	err = w.CurrentByName("Phoenix")
+	if err != nil { panic(err) }
+	fmt.Println(w.Weather)
 }
