3d4015ac0327dd01dddfe4f60e12a1e5e2307a7c
noarainstorm <100447165+noarainstorm@users.noreply.github.com>
2022-06-09 11:42:27 +0300

    Delete proxy (unused)

--- main.go
+++ main.go
@@ -3,23 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
-	"net/url"
 
 	owm "github.com/briandowns/openweathermap"
 )
 
-const proxy = "http://38.55.182.255:80"
 
 const apiKey = "c782b17fcc6669fa73afe6ba68e7f4f9"
 
 func main() {
-	i, err := url.Parse(proxy)
-	if err != nil {
-		panic(err)
-	}
-	cli := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(i)}}
-	w, err := owm.NewCurrent("C", "ru", apiKey, owm.WithHttpClient(cli))
+	w, err := owm.NewCurrent("C", "ru", apiKey)
 	if err != nil {
 		log.Fatalln(err)
 	}
