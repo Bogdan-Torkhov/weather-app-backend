7445ad6952e93a17fcf803016c7989afb18ac5f3
noarainstorm <100447165+noarainstorm@users.noreply.github.com>
2022-06-09 11:32:53 +0300

    Fix errors, delete unused comments

--- main.go
+++ main.go
@@ -1,34 +1,32 @@
 package main
 
 import (
-	"log"
 	"fmt"
-	// "os"
-    owm "github.com/briandowns/openweathermap"
-    "net/http"
-    "net/url"
-)
+	"log"
+	"net/http"
+	"net/url"
 
-// var apiKey = os.Getenv("OWM_API_KEY")
+	owm "github.com/briandowns/openweathermap"
+)
 
 const proxy = "http://38.55.182.255:80"
 
 const apiKey = "6ab7fc8050c5668c33576f3a93d1deea"
 
 func main() {
-<<<<<<< HEAD
 	i, err := url.Parse(proxy)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	cli := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(i)}}
-	w, err := owm.NewCurrent("C", "ru", apiKey, owm.WithHttpClient(cli)) // celseus (imperial) with Russian output
-=======
-	w, err := owm.NewCurrent("C", "RU", apiKey) // fahrenheit (imperial) with Russian output
->>>>>>> refs/remotes/origin/main
+	w, err := owm.NewCurrent("C", "ru", apiKey, owm.WithHttpClient(cli))
 	if err != nil {
 		log.Fatalln(err)
 	}
 	log.Println(apiKey)
 	err = w.CurrentByName("Phoenix")
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println(w.Weather)
 }
